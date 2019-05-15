c20417b6090005799dd42adb96097d17dfa66e58
Daniel Roberson <daniel@planethacker.net>
2019-05-15 14:51:07 -0700

    indentation

--- md5.c
+++ md5.c
@@ -7,37 +7,35 @@
 #include "md5.h"
 
 char *md5_digest_file(const char *path) {
-    unsigned char   c[MD5_DIGEST_LENGTH];
-    FILE            *fp;
-    int             bytes;
-    unsigned char   data[8192];
-    static char     digest[MD5_DIGEST_LENGTH * 2 + 1];
-    MD5_CTX         context;
+  unsigned char   c[MD5_DIGEST_LENGTH];
+  FILE            *fp;
+  int             bytes;
+  unsigned char   data[8192];
+  static char     digest[MD5_DIGEST_LENGTH * 2 + 1];
+  MD5_CTX         context;
 
-    fp = fopen(path, "rb");
-    if (fp == NULL) {
-        fprintf(stderr, "md5_digest_file: unable to open %s for reading: %s\n",
-                path,
-                strerror(errno));
-        return "";
-    }
+  fp = fopen(path, "rb");
+  if (fp == NULL) {
+    fprintf(stderr, "md5_digest_file: unable to open %s for reading: %s\n",
+	    path,
+	    strerror(errno));
+    return "";
+  }
 
-    MD5_Init(&context);
-    while ((bytes = fread(data, 1, sizeof(data), fp)) != 0) {
-        MD5_Update(&context, data, bytes);
-    }
-    MD5_Final(c, &context);
+  MD5_Init(&context);
+  while ((bytes = fread(data, 1, sizeof(data), fp)) != 0)
+    MD5_Update(&context, data, bytes);
+  MD5_Final(c, &context);
 
-    fclose(fp);
+  fclose(fp);
 
-    // TODO lol wtf is this
-    snprintf(digest, sizeof(digest),
-             "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-             c[0], c[1], c[2], c[3], c[4],
-             c[5], c[6], c[7], c[8], c[9],
-             c[10], c[11], c[12], c[13], c[14],
-             c[15]);
+  // TODO lol wtf is this
+  snprintf(digest, sizeof(digest),
+	   "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+	   c[0], c[1], c[2], c[3], c[4],
+	   c[5], c[6], c[7], c[8], c[9],
+	   c[10], c[11], c[12], c[13], c[14],
+	   c[15]);
 
-    return digest;
+  return digest;
 }
-
