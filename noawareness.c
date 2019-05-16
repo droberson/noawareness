e3172847b8837ef27c939636b043ad12c6b397a6
Daniel Roberson <daniel@planethacker.net>
2019-05-16 15:38:48 -0700

    inotify stuff pulled from inotify-watch is working!

--- noawareness.c
+++ noawareness.c
@@ -5,11 +5,14 @@
 #include <stdbool.h>
 #include <string.h>
 #include <limits.h>
+//#include <pwd.h>
+//#include <grp.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/inotify.h>
 #include <sys/select.h>
+#include <sys/stat.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -25,8 +28,11 @@
 #include "netlink_events.h"
 #include "time_common.h"
 #include "string_common.h"
+#include "inotify_common.h"
 
 // TODO syslog
+// TODO SIGHUP reload inotify.conf
+// TODO implement getgrgid and getpwuid myself because cant link these static
 // TODO map uids and gids to real names
 // TODO permissions, attributes, owner, groupships of files
 // TODO keep track of processes as they are executed to give EXIT better context
@@ -52,12 +58,14 @@
 /*
  * Globals
  */
-sock_t  sock;
-bool    daemonize = false;
-char    *pidfile = "/var/run/noawareness.pid";
-char    hostname[HOST_NAME_MAX];
-
-#define INOTIFY_BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+sock_t          sock;
+bool            daemonize = false;
+char            *pidfile = "/var/run/noawareness.pid";
+char            hostname[HOST_NAME_MAX];
+char            *inotifyconfig = NULL;
+char            *log_server = NULL;
+unsigned short	log_server_port = 55555;
+inotify_t       *head = NULL;
 
 
 void handle_netlink_message(struct cn_msg *cn_message) {
@@ -182,14 +190,6 @@ void select_netlink(int netlink, struct sockaddr_nl nl_kernel, struct cn_msg *cn
 }
 
 
-typedef struct inotify_entry {
-  int wd;
-  char filename[INOTIFY_BUF_LEN];
-  struct inotify_entry *next;
-} inotify_t;
-
-struct inotify_entry *head = NULL;
-
 void inotify_add(int wd, const char *filename) {
   inotify_t	*link = (inotify_t *)malloc(sizeof(inotify_t));
 
@@ -226,8 +226,166 @@ void inotify_remove(int wd) {
   }
 }
 
+void inotify_add_files(int fd, const char *path) {
+  int	wd;
+  FILE  *fp;
+  char  buf[INOTIFY_BUF_LEN];
+
+  // TODO add some sane defaults to this if no config file
+  // - /etc
+  // - /tmp, /var/tmp, /dev/shm
+  // - /root
+  // - /var/www/
+  if (path == NULL) {
+    fprintf(stderr, "no inotify config!\n");
+    return;
+  }
+
+  fp = fopen(path, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Unable to open inotify config file %s: %s\n",
+	    path, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // TODO add whether to look for just writes, creation, etc in config file.
+  while (fgets(buf, sizeof(buf), fp) != NULL) {
+    buf[strcspn(buf, "\n")] = '\0';
+    if (strlen(buf) == 0)
+      continue;
+
+    wd = inotify_add_watch(fd, buf, IN_ALL_EVENTS);
+    if (wd == -1)
+      fprintf(stderr, "inotify_add_watch %s: %s\n", buf, strerror(errno));
+    else
+      inotify_add(wd, buf);
+  }
+
+  fclose(fp);
+}
+
+void inotify_process_event(int inotify, struct inotify_event *e) {
+  int wd;
+  int found;
+  char output[1024];
+  char int2str[32];
+  char *mask;
+  char path[PATH_MAX];
+  char permstr[4];
+  inotify_t *current = head;
+  inotify_t *search;
+  int hash = 0, get_permissions = 0, check_dir = 0, remove = 0;
+  struct stat s;
+  //struct passwd *pwent;
+  //struct group *g;
+  char hashstr[] = "ahashyhashhash";
+
+  while (current->wd != e->wd)
+    current = current->next;
+
+  if (e->mask & IN_ACCESS)          mask = "IN_ACCESS";
+  if (e->mask & IN_ATTRIB)        { mask = "IN_ATTRIB"; get_permissions = 1; }
+  if (e->mask & IN_CLOSE_NOWRITE)   mask = "IN_CLOSE_NOWRITE";
+  if (e->mask & IN_CLOSE_WRITE)   { mask = "IN_CLOSE_WRITE"; hash = 1; }
+  if (e->mask & IN_CREATE)        { mask = "IN_CREATE"; get_permissions = 1; }
+  if (e->mask & IN_DELETE)          mask = "IN_DELETE";
+  if (e->mask & IN_DELETE_SELF)     mask = "IN_DELETE_SELF";
+  if (e->mask & IN_IGNORED)       { mask = "IN_IGNORED"; remove = 1; }
+  if (e->mask & IN_ISDIR)         { mask = "IN_ISDIR"; check_dir = 1; }
+  if (e->mask & IN_MODIFY)        { mask = "IN_MODIFY"; hash = 1; }
+  if (e->mask & IN_MOVE_SELF)       mask = "IN_MOVE_SELF";
+  if (e->mask & IN_MOVED_FROM)      mask = "IN_MOVED_FROM";
+  if (e->mask & IN_MOVED_TO)        mask = "IN_MOVED_TO";
+  if (e->mask & IN_OPEN)          { mask = "IN_OPEN"; get_permissions = 1; }
+  if (e->mask & IN_Q_OVERFLOW)      mask = "IN_Q_OVERFLOW";
+  if (e->mask & IN_UNMOUNT)         mask = "IN_UNMOUNT";
+
+  if (e->cookie > 0)
+    snprintf(int2str, sizeof(int2str), "%4d", e->cookie);
+
+  snprintf(path, sizeof(path), "%s%s%s",
+	   current->filename,
+	   (e->len > 0) ? "/" : "",
+	   (e->len > 0) ? e->name : "");
+
+  if (remove)
+    inotify_remove(e->wd);
+
+  if (check_dir) {
+    search = head;
+
+    for (found = 0; search->next; search = search->next) {
+      if (strcmp(search->filename, path) == 0) {
+	found = search->wd;
+	break;
+      }
+    }
+
+    if (found == 0) {
+      if (stat(path, &s) == 0) {
+	wd = inotify_add_watch(inotify, path, IN_ALL_EVENTS);
+	if (wd == -1) {
+	  fprintf(stderr, "inotify_add_watch %s: %s\n", path, strerror(errno));
+	} else {
+	  // TODO print new directory added
+	  inotify_add(wd, path);
+	}
+      }
+    }
+  } /* if (check_dir) */
+
+  char uid[16];
+  char gid[16];
+  if (get_permissions) {
+    if (stat(path, &s) == 0) {
+      //pwent = getpwuid(s.st_uid);
+      //g = getgrgid(s.st_gid);
+      snprintf(uid, sizeof(uid), "%d", s.st_uid);
+      snprintf(gid, sizeof(gid), "%d", s.st_gid);
+      snprintf(permstr, sizeof(permstr), "%o", s.st_mode);
+    }
+  }
+
+  //if (hash)
+    // do hash
+
+   snprintf(output, sizeof(output), "%s; wd =%2d; %s%s%s%s%s%s%s%s%s%s%s%s",
+	    path,
+	    e->wd,
+	    (e->cookie > 0) ? "cookie =" : "",
+	    (e->cookie > 0) ? int2str : "",
+	    (e->cookie > 0) ? "; " : "",
+	    mask,
+	    get_permissions || hash ? "; " : "",
+	    get_permissions || hash ? uid : "",
+	    //get_permissions || hash ? pwent->pw_name : "",
+	    get_permissions || hash ? ":" : "",
+	    get_permissions || hash ? gid : "",
+	    //get_permissions || hash ? g->gr_name : "",
+	    get_permissions ? "; perm = " : "",
+	    get_permissions ? permstr : "",
+	    hash ? " ; hash = " : "",
+	    hash ? hashstr : "");
+   printf("%s\n", output);
+}
+
 void select_inotify(int inotify) {
-  fprintf(stderr,"\n\n\nselect_inofify\n\n\n");
+  char *p;
+  int res;
+  char buf[INOTIFY_BUF_LEN];
+  struct inotify_event *event;
+
+  res = read(inotify, buf, sizeof(buf));
+  if ((res == 0) || (res == -1)) {
+    fprintf(stderr, "read() on inotify fd: %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  for (p = buf; p < buf + res; ) {
+    event = (struct inotify_event *) p;
+    inotify_process_event(inotify, event);
+    p += sizeof(struct inotify_event) + event->len;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -243,14 +401,28 @@ int main(int argc, char *argv[]) {
   enum proc_cn_mcast_op   *mcop_msg;
   fd_set                  fdset;
 
+
   /* Parse CLI options */
-  while((opt = getopt(argc, argv, "dp:h?")) != -1) {
+  while((opt = getopt(argc, argv, "dp:i:h?")) != -1) {
     switch (opt) {
     case 'd': /* Daemonize */
       daemonize = (daemonize == true) ? false : true;
       break;
-    case 'p': /* PID file location */
+
+    case 'p': /* Remote server port */
+      log_server_port = atoi(optarg);
+      break;
+
+    case 's': /* Remote server */
+      log_server = optarg;
+      break;
+
+    case 'P': /* PID file location */
       pidfile = optarg;
+      break;
+
+    case 'i': /* inotify config file location */
+      inotifyconfig = optarg;
       break;
 
       /* All of these effectively call usage(), so roll them over */
@@ -274,12 +446,7 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  int wd;    
-  wd = inotify_add_watch(inotify, "/usr/bin/id", IN_ALL_EVENTS);
-  if (wd == -1)
-    fprintf(stderr, "inotify_add_watch() %s : %s\n", "/tmp", strerror(errno));
-  else
-    inotify_add(wd, "/usr/bin/id");
+  inotify_add_files(inotify, inotifyconfig);
 
   /* Create netlink socket */
   netlink = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_CONNECTOR);
