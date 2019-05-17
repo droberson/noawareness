f035f98b2263fecea49f68a8ff7c97db5ab5a037
Daniel Roberson <daniel@planethacker.net>
2019-05-17 14:55:08 -0700

    add base64 stuff

--- md5.h
+++ md5.h
@@ -1,3 +1,5 @@
 #pragma once
 
 char *md5_digest_file(const char *);
+unsigned char *base64_encode(const unsigned char *, size_t, size_t *);
+unsigned char *base64_decode(const unsigned char *, size_t, size_t *);
