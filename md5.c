bfdfda39930277c593945b5de4bface28dd7286b
Daniel Roberson <daniel@planethacker.net>
2019-05-17 15:00:15 -0700

    remove line wrap code

--- md5.c
+++ md5.c
@@ -19,7 +19,6 @@ unsigned char *base64_encode(const unsigned char *src, size_t len, size_t *out_l
   unsigned char *out, *pos;
   const unsigned char *end, *in;
   size_t olen;
-  //int line_len;
 
   olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
   olen += olen / 72; /* line feeds */
@@ -33,18 +32,13 @@ unsigned char *base64_encode(const unsigned char *src, size_t len, size_t *out_l
   end = src + len;
   in = src;
   pos = out;
-  //line_len = 0;
+
   while (end - in >= 3) {
     *pos++ = base64_table[in[0] >> 2];
     *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
     *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
     *pos++ = base64_table[in[2] & 0x3f];
     in += 3;
-    //line_len += 4;
-    //if (line_len >= 72) {
-    //  *pos++ = '\n';
-    //  line_len = 0;
-    //}
   }
 
   if (end - in) {
@@ -58,11 +52,7 @@ unsigned char *base64_encode(const unsigned char *src, size_t len, size_t *out_l
       *pos++ = base64_table[(in[1] & 0x0f) << 2];
     }
     *pos++ = '=';
-    //line_len += 4;
   }
-
-  //if (line_len)
-  //  *pos++ = '\n';
 
   *pos = '\0';
   if (out_len)
